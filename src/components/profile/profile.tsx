f97b42af3eae57a61546bcbdad35395d59fbcfcf
Ayush Sharma <64184220+gladiatortony7@users.noreply.github.com>
2022-07-08 15:38:34 +0530

    profile app

---
+++ src/components/profile/profile.tsx
@@ -0,0 +1,20 @@
+import React,{Component} from 'react';
+import "./profile.css";
+
+export class Profile extends Component {
+
+    render(){
+        return(
+            <div className="try">
+
+            <h1> Profile </h1>
+            <h3>Name         :    Ayush</h3>
+            <h3>Father's name:    Anil kumar</h3>
+            <h3>College      :    Arya college of engineering and researh center</h3>
+            <h3>Education    :    Computer science enigineering</h3>
+
+
+            </div>
+        )
+    }
+}
---
+++ src/index2.js
@@ -0,0 +1,5 @@
+import React from "react";
+import { ReactDOM } from 'react-dom';
+
+const element= <h1>hello world</h1>;
+console.log(element);
